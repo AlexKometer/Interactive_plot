2cddde57836dba4905e4fb5ea73a307b8278e600
AlexKometer <a.kometer@mci4me.at>
2024-05-16 15:21:54 +0200

    added calculations in Power tab

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 df1 = pd.read_csv("data/ekg_data/01_Ruhe.txt", sep="\t", header=None)
 
 df2 = pd.read_csv("data/activities/activity.csv")
@@ -13,7 +12,6 @@ df2 = pd.read_csv("data/activities/activity.csv")
 t_end = len(df2["PowerOriginal"])
 
 df2["Time in s"] = np.arange(0, t_end)
-HR_max = df2["HeartRate"].max ()
 
 path1 = "data/ekg_data/01_Ruhe.txt"
 path2 = "data/ekg_data/02_Ruhe.txt"
@@ -22,7 +20,6 @@ path4 = "data/ekg_data/04_Belastung.txt"
 path5 = "data/ekg_data/05_Belastung.txt"
 
 paths = [path1, path2, path3, path4, path5]
-
 
 tab1, tab2 = st.tabs(["EKG-Data", "Power-Data"])
 
@@ -37,8 +34,32 @@ with tab1:
 with tab2:
     st.subheader("Interaktiver Plot")
     st.header("Power- & Heart-Data")
-    fig = make_plot_power(df2)
+    hr_max = st.number_input('Please enter the maximum heartrate (0 = no Input -> max_hr generated from data):',
+                             min_value=0, max_value=300, value=0, step=1)
+    if hr_max == 0:
+        hr_max = df2["HeartRate"].max()
+    elif hr_max < df2["HeartRate"].max():
+        st.markdown("The maximum heartrate from data is:", df2["HeartRate"].max())
+        st.markdown("Are you sure you want to use a lower value?")
+
+    fig, zone_counts = make_plot_power(df2, hr_max)
+
+    st.write("Maximum power:", round(df2["PowerOriginal"].max()), "watts")
+    st.write("Average power:", round(df2["PowerOriginal"].mean()), "watts")
+
     st.plotly_chart(fig)
 
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Time in Zone 1:", zone_counts["Zone1"], "s")
+        st.write("Time in Zone 2:", zone_counts["Zone2"], "s")
+        st.write("Time in Zone 3:", zone_counts["Zone3"], "s")
+        st.write("Time in Zone 4:", zone_counts["Zone4"], "s")
+        st.write("Time in Zone 5:", zone_counts["Zone5"], "s")
 
-
+    with col2:
+        zones = ['Zone1', 'Zone2', 'Zone3', 'Zone4', 'Zone5']
+        for zone in zones:
+            if zone in df2.columns and df2[zone].any():
+                average_power = round(df2[df2[zone]]['PowerOriginal'].mean())
+                st.write(f"Average power in {zone}:", average_power, "watts")
--- read_pandas.py
+++ read_pandas.py
@@ -1,41 +1,72 @@
 import pandas as pd
 import plotly.express as px
-
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 def read_my_csv(path):
     df = pd.read_csv(path, sep="\t", header=None)
-    df.columns = ["Messwerte in mV","Zeit in ms"]
+    df.columns = ["Messwerte in mV", "Zeit in ms"]
 
     return df
+
 
 def make_plot_ecg(df):
     # Creating a line plot using Plotly Express
     fig = px.line(df, x="Zeit in ms", y="Messwerte in mV")
 
     # Configuring the layout to include a range slider
-    fig.update_layout(xaxis=dict(rangeslider=dict(visible=True,thickness=0.1),type='linear',fixedrange=False),yaxis=dict(fixedrange=True ))
+    fig.update_layout(xaxis=dict(rangeslider=dict(visible=True, thickness=0.1), type='linear', fixedrange=False),
+                      yaxis=dict(fixedrange=True))
     return fig
 
-def make_plot_power(df2):
-    fig = px.line (df2,x="Time in s",y=["PowerOriginal", "HeartRate"],color_discrete_sequence=["lightgray","royalblue"])
 
-    HR_max = df2["HeartRate"].max ()
-    zone1 = HR_max * 0.6
-    zone2 = HR_max * 0.7
-    zone3 = HR_max * 0.8
-    zone4 = HR_max * 0.9
+def make_plot_power(df2, hr_max):
+    fig = px.line(df2, x="Time in s", y=["PowerOriginal", "HeartRate"],
+                  color_discrete_sequence=["lightgray", "royalblue"])
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(
+        go.Scatter(x=df2["Time in s"], y=df2["PowerOriginal"], name="PowerOriginal", line=dict(color="lightgray")),
+        secondary_y=False,
+    )
+    fig.add_trace(
+        go.Scatter(x=df2["Time in s"], y=df2["HeartRate"], name="HeartRate", line=dict(color="royalblue")),
+        secondary_y=True,
+    )
+    fig.update_xaxes(title_text="Time in s")
+
+    fig.update_yaxes(title_text="Power (watts)", title_font=dict(color="lightgray"), tickfont=dict(color="lightgray"),
+                     secondary_y=False)
+    fig.update_yaxes(title_text="Heart Rate (bpm)", title_font=dict(color="royalblue"),
+                     tickfont=dict(color="royalblue"), secondary_y=True)
+
+    max_value = max(df2["PowerOriginal"].max(), df2["HeartRate"].max())
+    fig.update_yaxes(range=[0, max_value], secondary_y=False)
+    fig.update_yaxes(range=[0, max_value], secondary_y=True)
+
+    zone1 = hr_max * 0.6
+    zone2 = hr_max * 0.7
+    zone3 = hr_max * 0.8
+    zone4 = hr_max * 0.9
 
     df2["Zone1"] = df2["HeartRate"] < zone1
     df2["Zone2"] = (df2["HeartRate"] >= zone1) & (df2["HeartRate"] < zone2)
     df2["Zone3"] = (df2["HeartRate"] >= zone2) & (df2["HeartRate"] < zone3)
     df2["Zone4"] = (df2["HeartRate"] >= zone3) & (df2["HeartRate"] < zone4)
-    df2["Zone5"] = df2["HeartRate"] >= zone4
+    df2["Zone5"] = (df2["HeartRate"] >= zone4) & (df2["HeartRate"] < hr_max)
 
-    fig.add_hrect (y0=0, y1=zone1, fillcolor="lightgreen", opacity=0.5, layer="below", line_width=0)
-    fig.add_hrect (y0=zone1, y1=zone2, fillcolor="green", opacity=0.5, layer="below", line_width=0)
-    fig.add_hrect (y0=zone2, y1=zone3, fillcolor="yellow", opacity=0.5, layer="below", line_width=0)
-    fig.add_hrect (y0=zone3, y1=zone4, fillcolor="orange", opacity=0.5, layer="below", line_width=0)
-    fig.add_hrect (y0=zone4, y1=HR_max, fillcolor="red", opacity=0.5, layer="below", line_width=0)
+    zone_counts = {
+        "Zone1": df2["Zone1"].sum(),
+        "Zone2": df2["Zone2"].sum(),
+        "Zone3": df2["Zone3"].sum(),
+        "Zone4": df2["Zone4"].sum(),
+        "Zone5": df2["Zone5"].sum(),
+    }
 
-    return fig
+    fig.add_hrect(y0=0, y1=zone1, fillcolor="lightgreen", opacity=0.5, layer="below", line_width=0)
+    fig.add_hrect(y0=zone1, y1=zone2, fillcolor="green", opacity=0.5, layer="below", line_width=0)
+    fig.add_hrect(y0=zone2, y1=zone3, fillcolor="yellow", opacity=0.5, layer="below", line_width=0)
+    fig.add_hrect(y0=zone3, y1=zone4, fillcolor="orange", opacity=0.5, layer="below", line_width=0)
+    fig.add_hrect(y0=zone4, y1=hr_max, fillcolor="red", opacity=0.5, layer="below", line_width=0)
+
+    return fig, zone_counts
